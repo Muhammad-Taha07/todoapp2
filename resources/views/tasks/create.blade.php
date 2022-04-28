0501ba358d99f60edcd1653fa56a30d3992ea112
najeeullah <najeeullah@code-avenue.com>
2022-04-28 15:09:49 +0500

    View: Addition of CSS & Fixed headings.

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,15 +2,16 @@
 @section('content')
 
     <h1>New Task</h1>
+    <hr class="solid" style="border-top: 3px solid #bbb;">
     <form action="/tasks" method="POST">
         @csrf
         <div class="form-group">
             <label for="description">Task Description</label>
             <input class="form-control" name="description" required/>
         </div>
-
         <div class="form-group">
             <button type="submit" class="btn btn-primary" style="margin-top: 20px;">Create Task</button>
         </div>
+        <hr class="solid" style="border-top: 3px solid #bbb;">
     </form>
     @endsection
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,16 +2,17 @@
     @section('content')
 
     <h1 style="margin-top: 30px;">To Do List Application</h1>
-    <h3 style="margin-top: 20px; color: green;">Your Task(s)</h1>
+    <hr class="solid" style="border-top: 3px solid #bbb;">
     <a href="/tasks/create" class="btn btn-primary btn-lg btn-block" style="margin-bottom: 20px;">Add a new Task</a>
+    {{-- <h3 style="margin-top: 20px; color: green;">Your Task(s)</h1> --}}
+    <hr class="solid" style="border-top: 3px solid #bbb;">
 
     @foreach($tasks as $task)
 
     <div class="card @if($task->isCompleted()) border-success @endif" style="margin-bottom: 20px;">
     <div class="card-body">
-    <p>{{$task->description}}</p>
-{{-- <span class="badge badge-success" style="color: rgb(8, 141, 41);">Completed</span> --}}
 
+    <p style="font-size: 25px;">{{$task->description}}</p>
     @if (!$task->isCompleted())
     <form action="/tasks/{{$task->id}}" method="POST">
         @method('PATCH')
@@ -22,6 +23,7 @@
     @else
 
     <form action="/tasks/{{$task->id}}" method="POST">
+        <h3 style="margin-top: 20px; color: green; text-shadow: 2px 2px 5px;">Task Completed</h1>
         @method('DELETE')
         @csrf
         <button class="btn btn-danger" input="submit" style="color: rgb(234, 234, 234);">Delete Task</button>
